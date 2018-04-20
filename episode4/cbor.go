f501453276d8bd0bfa469ccbb3cb69bc511b8b76
Henry Precheur <henry@precheur.org>
2018-04-19 21:46:38 -0700

    episode 4 wip

---
+++ episode4/cbor.go
@@ -0,0 +1,108 @@
+// Implements CBOR encoding:
+//
+//   https://tools.ietf.org/html/rfc7049
+//
+package cbor
+
+import (
+	"encoding/binary"
+	"errors"
+	"io"
+	"reflect"
+)
+
+type Encoder struct {
+	w io.Writer
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	return &Encoder{w: w}
+}
+
+var ErrNotImplemented = errors.New("Not Implemented")
+
+func (e *Encoder) writeHeader(major, minor byte) error {
+	h := byte((major << 5) | minor)
+	_, err := e.w.Write([]byte{h})
+	return err
+}
+
+// writeHeaderInteger writes out a header created from major and minor magic
+// numbers and write the value v as a big endian value
+func (e *Encoder) writeHeaderInteger(major, minor byte, v interface{}) error {
+	if err := e.writeHeader(major, minor); err != nil {
+		return err
+	}
+	return binary.Write(e.w, binary.BigEndian, v)
+}
+
+func (e *Encoder) writeInteger(i uint64) error {
+	switch {
+	case i <= 23:
+		return e.writeHeader(majorPositiveInteger, byte(i))
+	case i <= 0xff:
+		return e.writeHeaderInteger(
+			majorPositiveInteger, minorPositiveInt8, uint8(i),
+		)
+	case i <= 0xffff:
+		return e.writeHeaderInteger(
+			majorPositiveInteger, minorPositiveInt16, uint16(i),
+		)
+	case i <= 0xffffffff:
+		return e.writeHeaderInteger(
+			majorPositiveInteger, minorPositiveInt32, uint32(i),
+		)
+	default:
+		return e.writeHeaderInteger(
+			majorPositiveInteger, minorPositiveInt64, uint64(i),
+		)
+	}
+}
+
+func (e *Encoder) Encode(v interface{}) error {
+	x := reflect.ValueOf(v)
+	switch x.Kind() {
+	case reflect.Invalid:
+		// naked nil value == invalid type
+		return e.writeHeader(majorSimpleValue, simpleValueNil)
+	case reflect.Ptr:
+		if x.IsNil() {
+			return e.writeHeader(majorSimpleValue, simpleValueNil)
+		} else {
+			// return e.Encode(reflect.Indirect(x).Interface())
+		}
+	case reflect.Bool:
+		var minor byte
+		if x.Bool() {
+			minor = simpleValueTrue
+		} else {
+			minor = simpleValueFalse
+		}
+		return e.writeHeader(majorSimpleValue, minor)
+	case
+		reflect.Uint,
+		reflect.Uint8,
+		reflect.Uint16,
+		reflect.Uint32,
+		reflect.Uint64:
+		return e.writeInteger(x.Uint())
+	}
+	return ErrNotImplemented
+}
+
+const (
+	// major types
+	majorPositiveInteger = 0
+	majorSimpleValue     = 7
+
+	// extended integers
+	minorPositiveInt8  = 24
+	minorPositiveInt16 = 25
+	minorPositiveInt32 = 26
+	minorPositiveInt64 = 27
+
+	// simple values == major type 7
+	simpleValueFalse = 20
+	simpleValueTrue  = 21
+	simpleValueNil   = 22
+)
---
+++ episode4/cbor_test.go
@@ -0,0 +1,93 @@
+package cbor
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+)
+
+// testEncoder test the CBOR encoder with the value v, and verify that err, and
+// expected match what's returned and written by the encoder.
+func testEncoder(t *testing.T, v interface{}, err error, expected []byte) {
+	// buffer is where we write the CBOR encoded values
+	var buffer = bytes.Buffer{}
+	// create a new encoder writing to buffer, and encode v with it
+	var e = NewEncoder(&buffer).Encode(v)
+
+	if e != err {
+		t.Fatalf("err: %#v != %#v with %#v", e, err, v)
+	}
+
+	if !bytes.Equal(buffer.Bytes(), expected) {
+		t.Fatalf(
+			"(%#v) %#v != %#v", v, buffer.Bytes(), expected,
+		)
+	}
+}
+
+func TestNil(t *testing.T) {
+	testEncoder(t, nil, nil, []byte{0xf6})
+}
+
+func TestNilTyped(t *testing.T) {
+	var i *int = nil
+	testEncoder(t, i, nil, []byte{0xf6})
+
+	var v interface{} = nil
+	testEncoder(t, v, nil, []byte{0xf6})
+}
+
+func TestPointer(t *testing.T) {
+	i := 10
+	pi := &i
+
+	testEncoder(t, pi, nil, []byte{0x0a})
+}
+
+func TestBool(t *testing.T) {
+	testEncoder(t, false, nil, []byte{0xf4})
+	testEncoder(t, true, nil, []byte{0xf5})
+}
+
+func TestIntSmall(t *testing.T) {
+	for i := 0; i <= 23; i++ {
+		var expected = []byte{byte(i)}
+		testEncoder(t, uint64(i), nil, expected)
+	}
+}
+
+func TestIntBig(t *testing.T) {
+	var cases = []struct {
+		Value    uint64
+		Expected []byte
+	}{
+		// Examples from CBOR spec
+		{Value: 0, Expected: []byte{0x00}},
+		{Value: 1, Expected: []byte{0x01}},
+		{Value: 10, Expected: []byte{0x0a}},
+		{Value: 23, Expected: []byte{0x17}},
+		{Value: 24, Expected: []byte{0x18, 0x18}},
+		{Value: 25, Expected: []byte{0x18, 0x19}},
+		{Value: 100, Expected: []byte{0x18, 0x64}},
+		{Value: 1000, Expected: []byte{0x19, 0x03, 0xe8}},
+		{Value: 1000000, Expected: []byte{0x1a, 0x00, 0x0f, 0x42, 0x40}},
+		{
+			Value: 1000000000000,
+			Expected: []byte{
+				0x1b, 0x00, 0x00, 0x00, 0xe8, 0xd4, 0xa5, 0x10, 0x00,
+			},
+		},
+		{
+			Value: 18446744073709551615,
+			Expected: []byte{
+				0x1b, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%d", c.Value), func(t *testing.T) {
+			testEncoder(t, uint64(c.Value), nil, c.Expected)
+		})
+	}
+}
