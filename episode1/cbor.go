867ead82295a77c3b33c0b37d0150dad2999c0dd
Henry Precheur <henry@precheur.org>
2018-05-23 17:03:24 -0700

    gofmt

--- episode1/cbor.go
+++ episode1/cbor.go
@@ -5,26 +5,26 @@
 package cbor
 
 import (
-        "io"
-		"errors"
+	"errors"
+	"io"
 )
 
 type Encoder struct {
-        w   io.Writer
+	w io.Writer
 }
 
 func NewEncoder(w io.Writer) *Encoder {
-        return &Encoder{w: w}
+	return &Encoder{w: w}
 }
 
 var ErrNotImplemented = errors.New("Not Implemented")
 
 // Can only encode nil
 func (enc *Encoder) Encode(v interface{}) error {
-    switch v.(type) {
-    case nil:
-        var _, err = enc.w.Write([]byte{0xf6})
-        return err
-    }
-    return ErrNotImplemented
+	switch v.(type) {
+	case nil:
+		var _, err = enc.w.Write([]byte{0xf6})
+		return err
+	}
+	return ErrNotImplemented
 }
--- episode1/cbor_test.go
+++ episode1/cbor_test.go
@@ -1,18 +1,18 @@
 package cbor
 
 import (
-    "bytes"
-    "testing"
+	"bytes"
+	"testing"
 )
 
 func TestNil(t *testing.T) {
-    var buffer = bytes.Buffer{}
-    var err = NewEncoder(&buffer).Encode(nil)
+	var buffer = bytes.Buffer{}
+	var err = NewEncoder(&buffer).Encode(nil)
 
-    if !(err == nil && bytes.Equal(buffer.Bytes(), []byte{0xf6})) {
-        t.Fatalf(
-            "%#v != %#v or %#v != %#v",
-            err, nil, buffer.Bytes(), []byte{0xf6},
-        )
-    }
+	if !(err == nil && bytes.Equal(buffer.Bytes(), []byte{0xf6})) {
+		t.Fatalf(
+			"%#v != %#v or %#v != %#v",
+			err, nil, buffer.Bytes(), []byte{0xf6},
+		)
+	}
 }
